a78f6751036ba4d383c423c9ea76a407212bcee0
Leo Idraikh <loiyedreekh@live.com>
2018-09-04 22:30:23 -0400

    Projct Progress to lecutre 55
    update templates for shopinglist, recipes and recipe details ingredient model, shared folder and more

--- proj-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ proj-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test','This is testing','https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg');
+    new Recipe('A Test',
+    'This is testing',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
+    new Recipe('A Test',
+    'This is testing',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
   ];
   constructor() { }
 
---
+++ proj-app/src/app/shared/indredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    public name: string;
+    public amount: number;
+
+    constructor(name,amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+    //other way
+    // constructor(public name:string, public amount:number) {}
+}
